4a05ae4d8499986efae06c9ee776c4366efcd38b
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2022-03-30 20:15:54 +0200

    Improvements

--- src/Main.php
+++ src/Main.php
@@ -6,13 +6,57 @@ use Sokoloowski\Lexer;
 
 class Main
 {
-    public static function main() : void
+    public static function main(): void
     {
-        $lex = new Lexer("input string");
+        $tokens = [];
+        $lex = new Lexer("<?php 512<61.23+132-123*432>2435/5454<=56>=34");
 
         do {
-            echo $lex->consume();
+            $char = $lex->consume();
+
+            if (preg_match("/\d/", $char)) {
+                while (preg_match("/\d/", $lex->peek())) {
+                    $lex->consume();
+                }
+                if ($lex->peek() === ".") {
+                    $lex->consume();
+                    if (!preg_match("/\d/", $lex->peek())) {
+                        die(sprintf("Expected digit on line %d at position %d\n", $lex->getLine(), $lex->getColumn()));
+                    }
+                    
+                    while (preg_match("/\d/", $lex->peek())) {
+                        $lex->consume();
+                    }
+
+                    $tokens[] = "float ";
+                } else {
+                    $tokens[] = "integer ";
+                }
+            } elseif ($char === "<") {
+                if ($lex->peek() === "?") {
+                    if (sprintf("%s%s%s", $lex->peek(1), $lex->peek(2), $lex->peek(3)) === "php") {
+                        $lex->consume();
+                        $lex->consume();
+                        $lex->consume();
+                        $lex->consume();
+                        if (!preg_match("/\s/", $lex->consume())) {
+                            die(sprintf("Expected \" \" on line %d at position %d\n", $lex->getLine(), $lex->getColumn()));
+                        }
+                        $tokens[] = "php_opening_tag ";
+                    }
+                } elseif ($lex->peek() === " " || $lex->peek() === "=" || preg_match("/\d/", $lex->peek())) {
+                    $tokens[] = "math_operator ";
+                }
+            } elseif (preg_match("/[\+\-\/\*\>]/", $char)) {
+                if ($lex->peek() === " " || (preg_match("/[<>]/", $char) && $lex->peek() === "=") || preg_match("/\d/", $lex->peek())) {
+                    $tokens[] = "math_operator ";
+                }
+            }
+
+            // echo sprintf("--- %s ---", $lex);
         } while (!$lex->isEOF());
+
+        echo implode(" ", $tokens);
 
         echo PHP_EOL;
     }
--- src/Sokoloowski/Lexer.php
+++ src/Sokoloowski/Lexer.php
@@ -2,26 +2,63 @@
 
 namespace Sokoloowski;
 
+use Exception;
+
 /**
  * https://ichi.pro/pl/pisanie-parsera-czesc-i-pierwsze-kroki-75567545937097
  */
 class Lexer
 {
     private string $input;
+    private int $column;
+    private int $line;
 
     public function __construct(string $input)
     {
         $this->input = $input;
+        $this->column = 1;
+        $this->line = 1;
+    }
+
+    public function __toString()
+    {
+        return $this->input;
+    }
+
+    public function getColumn() : int
+    {
+        return $this->column;
+    }
+
+    public function getLine() : int
+    {
+        return $this->line;
     }
 
     public function peek(int $k = 0) : string
-    {        
-        return $this->input[$k];
+    {
+        try {
+            return $this->input[$k];
+        } catch (Exception $e) {
+            return "";
+        }
     }
 
     public function consume(int $k = 0) : string
     {
-        $char = $this->input[$k];
+        try {
+            $char = $this->input[$k];
+        } catch (Exception $e) {
+            return "";
+        }
+
+        if ($char === "\n") {
+            $this->line++;
+            $this->column = -1;
+        } else {
+            $this->column++;
+        }
+
         $this->input = substr($this->input, 0, $k) . substr($this->input, $k + 1);
     
         return $char;
